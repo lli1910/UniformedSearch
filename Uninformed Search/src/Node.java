728a8fc12114fc9bec03c288df40c1738deb4df6
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-05-31 09:19:00 -0400

    9:28 AM 5/31/18

--- Uninformed Search/src/Node.java
+++ Uninformed Search/src/Node.java
@@ -1,6 +1,34 @@
+import java.util.ArrayList;
+
 //Author: Lily Li
 //Date created: 5/31/18
 
 public class Node { 
+	public String stationName;
+	Node leftChild;
+	Node rightChild;
+	private ArrayList<Node> children;
+	
+	//constructor
+	public Node(String stationName) {
+		
+	}
+	
+	//return a list of all connected nodes
+	public ArrayList<Node> getChildren(){
+		return null;
+	}
+	
+	//used to set the children of a node
+	public void setChildren (ArrayList<Node> children) {
+		
+		
+	}
+	
+	//print the path found
+	public String toString() {
+		return null;
+	}
+	
 
 }
